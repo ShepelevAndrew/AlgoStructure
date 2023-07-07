08c3405144f696b65f1b20941a236382441f908b
Andrew <shepelev.andrew.101@gmail.com>
2023-07-07 12:28:11 +0300

    start

---
+++ src/Core/Algorithm/AlgorithmBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.Algorithm;
+
+public class AlgorithmBase<T>
+{
+    public List<T> Items { get; set; }
+
+    public AlgorithmBase(List<T> items)
+    {
+        Items = items;
+    }
+
+    public virtual void Sort() {
+        Items.Sort();
+    }
+
+    protected void Swop(int posA, int posB) {
+        if(posA < Items.Count && posB < Items.Count) {
+            var temp = Items[posA];
+            Items[posA] = Items[posB];
+            Items[posB] = temp;
+        }
+    }
+}
---
+++ src/Core/Algorithm/BubbleSort.cs
@@ -0,0 +1,11 @@
+namespace Core.Algorithm;
+
+public class BubbleSort<T> : AlgorithmBase<T>
+{
+    public BubbleSort(List<T> list) : base(list) {}
+
+    public override void Sort()
+    {
+        
+    }    
+}
