b09017a64dbc72b9db09be8e977fdf4d9188c0c3
Andrew <shepelev.andrew.101@gmail.com>
2023-07-08 13:34:12 +0300

    Shell sort

---
+++ src/Core/Algorithm/ShellSort.cs
@@ -0,0 +1,22 @@
+namespace Core.Algorithm;
+
+public class ShellSort<T> : AlgorithmBase<T> where T: IComparable<T>
+{
+    public ShellSort(List<T> list) : base(list) { }
+
+    public override void Sort()
+    {
+        int step = Items.Count / 2;
+
+        while(step > 0) {
+            for(int i = step; i < Items.Count; i++) {
+                int j = i;
+                while(j >= step && Items[j - step].CompareTo(Items[j]) == 1) {
+                    Swop(j - step, j);
+                    j -= step;
+                }
+            }
+            step /= 2;
+        }
+    }
+}
